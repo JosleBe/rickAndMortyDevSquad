0e0239a3287367d3b9e8d347d2d7fb86b720e020
Jose Leonardo Martinez Gomez <20223tn067@utez.edu.mx>
2025-05-06 15:24:32 -0600

    Agregar servicio

---
+++ service.js
@@ -0,0 +1,42 @@
+import BASE_URL from "./app.config.js"
+
+
+const rickAndMortyService = {
+    getByStatus: async (status) => {
+        try {
+            const response = await fetch(`${BASE_URL}?status=${status}`);
+
+            if (!response.ok) return console.log("Error de peticion")
+            
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error("Error al obtener los datos", error);
+        }
+    },
+    getbySpecies: async (species) => { 
+        try{
+            const response = await fetch(`${BASE_URL}?species=${species}`);
+
+            if (!response.ok) return console.log("Error de peticion");
+            const data = await response.json();
+            return data;
+        }catch (error) {
+            console.log("Error al obtner los datos", error);
+        }
+    },
+    getByGenders: async (gender) => {
+        try {
+            const response = await fetch(`${BASE_URL}?gender=${gender}`);
+            if(!response.ok) return console.log("Error de peticion");
+            const data = await  response.json();
+            return data;
+        } catch (error) {
+            console.log("Error al obtener los datos", error);
+        }
+    } 
+};
+
+console.log( await rickAndMortyService.getByStatus("Dead") );
+console.log( await rickAndMortyService.getByGenders("Male") );
+console.log( await rickAndMortyService.getbySpecies("Human"));
